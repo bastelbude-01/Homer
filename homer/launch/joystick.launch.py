0f676db239855ac36476578f1b5725acda32030e
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2024-02-01 21:46:51 +0100

    ready for homer rc

--- homer/launch/joystick.launch.py
+++ homer/launch/joystick.launch.py
@@ -10,9 +10,8 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    joy_params1 = os.path.join(get_package_share_directory("leo"), "config", "joystick.yaml")
-    joy_params2 = os.path.join(get_package_share_directory("leo"), "config", "joystickTurret.yaml")
-    pkg_path = os.path.join(get_package_share_directory('leo'))
+    joy_params1 = os.path.join(get_package_share_directory("homer"), "config", "joystick.yaml")
+    pkg_path = os.path.join(get_package_share_directory('homer'))
 
 
     joystick = Node(
@@ -29,42 +28,28 @@ def generate_launch_description():
             remappings=[("/cmd_vel", "/leo_driver/cmd_vel_unstamped")]
         )
 
-    teleop_node2 = Node(
-            package='teleop_twist_joy',
-            executable='teleop_node',
-            name='teleop_node2',
-            parameters=[joy_params2],
-            remappings=[("/cmd_vel", "/leo_turret")]
-        )
 
     rviz2 = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="screen",
-        arguments=["-d", os.path.join(pkg_path,"rviz","display_leo.rviz" )],
+        arguments=["-d", os.path.join(pkg_path,"rviz","display_homer.rviz" )],
     )
 
-    snow_blower = Node(
-        package="rc_control",
-        executable="snowblower",
-        name="snowblower_controller"
-    )
 
 
 
 
     return LaunchDescription([
 
-        launch.actions.ExecuteProcess(
-            cmd=['ros2', 'run', 'micro_ros_agent','micro_ros_agent','udp4','--port', '8868', '--dev', '192.168.178.88'],
-            output='screen',
-        ),
+        # launch.actions.ExecuteProcess(
+        #     cmd=['ros2', 'run', 'micro_ros_agent','micro_ros_agent','udp4','--port', '8868', '--dev', '192.168.178.88'],
+        #     output='screen',
+        # ),
 
         joystick,
         teleop_node1,
-        teleop_node2,
-        rviz2,
-        snow_blower
+        rviz2
         
     ])
